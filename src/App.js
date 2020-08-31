484f161e0571838806eefb297d8b91324ee7cb41
Hnoss33 <52913599+Hnoss33@users.noreply.github.com>
2020-09-01 09:03:12 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react';
 import { MenuItem, FormControl, Select } from "@material-ui/core"
 import './App.css';
+
 // State = How to write a variable in REACT 
 //https://disease.sh/v3/covid-19/countries
 //useEffect = runes a piece of code based on a given condition
-
-function App() { //cada vez que este componente do load on the screen el useeffect corre y verifica abajo en los braquets so if changes the code get load again  
-  const [countries, setCountries] = useState([]); //este es el HOOK 'useState'
-useEffect(() => {
 //the code incide here will run once when the component loads and not again after, solo una vez corre esto
 //async -> send a request, wait for it, do something with info
-const getCountriesData = async () => {
-    await fetch ("https://disease.sh/v3/covid-19/countries")
-    .then((response) => response.json())
-    .then((data) => { //when this Data comeback we have to deestructure this shit, so 
-            const countries = data.map((country) => ({
-              name: country.country,
-              value: country.countryInfo.iso2 
-             }))
-        })
-    }
-  }, []); //si esto en los braquets no se completa habra un loop infinito debe haber 
+//cada vez que este componente do load on the screen el useeffect corre y verifica abajo en los braquets so if changes the code get load again 
+function App() {  
+  const [countries, setCountries] = useState([]); //este es el HOOK 'useState'
+
+      useEffect(() => {
+        const getCountriesData = async () => {
+          await fetch ("https://disease.sh/v3/covid-19/countries")
+            .then((response) => response.json())
+            .then((data) => { //when this Data comeback we have to deestructure this shit, so 
+                    const countries = data.map((country) => ({
+                    name: country.country,
+                    value: country.countryInfo.iso2, 
+              }));
+              setCountries(countries)
+           })
+          }
+  }, []); //si esto en los braquets no se completa habra un loop infinito debe haber algo aca que determine que solo se hara para 1 solo elemento 
   return (
     <div className="app">
     <div className="app__header">
@@ -28,7 +31,7 @@ const getCountriesData = async () => {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {countries.map((country) => (
-              <MenuItem value={country}></MenuItem>
+              <MenuItem value={country}>{country}</MenuItem>
             ))}
           </Select>
         </FormControl>
